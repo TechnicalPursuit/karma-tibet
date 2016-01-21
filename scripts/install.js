a2c9cbf26fae472ed65b71677639a1b42bb85748
idearat <ss@technicalpursuit.com>
2016-01-20 19:00:52 -0700

    Upgrade search logic for destination for karma.conf.js file.

--- scripts/install.js
+++ scripts/install.js
@@ -13,10 +13,34 @@
     sh = require('shelljs');
 
     src = path.join(__dirname, '..', 'karma.conf.js');
-    dest = path.join(__dirname, '../../..', 'karma.conf.js');
+
+    // Since the CLI can be invoked from anywhere we need to be explicit here
+    // relative to the cwd. If we find a project file, and it's 'tibet' we're
+    // truly _inside_ the library.
+    dir = __dirname;
+    file = 'package.json';
+    while (dir.length > 0) {
+        fullpath = path.join(dir, file);
+        if (sh.test('-f', fullpath)) {
+            //  First one we find will be the karma-tibet module itself. Keep
+            //  going until we find the "consumer" of this module.
+            if (require(fullpath).name !== 'karma-tibet') {
+                dest = dir;
+                break;
+            }
+        }
+        dir = dir.slice(0, dir.lastIndexOf(path.sep));
+    }
+
+    if (!dest) {
+        console.error('Unable to locate project root directory.');
+        process.exit(0);
+    }
 
     console.log('Installing karma-tibet configuration file karma.conf.js.');
 
+    //  Update to check for the target configuration file, not just directory.
+    dest = path.join(dest, 'karma.conf.js');
     if (sh.test('-e', dest)) {
         console.log('File karma.conf.js already exists; copying it to *.orig.');
 
